43e0c29bd0166e66340dead7caf26f63c5249188
emmovsixty <emmo1933@gmail.com>
2022-08-04 12:38:39 +0700

    ubah latihanInout.java

--- src/latihanInput.java
+++ src/latihanInput.java
@@ -1,17 +1,27 @@
-
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
-
 
 public class latihanInput {
     public static void main(String[] args) {
-        ArrayList<String> dataNama = new ArrayList<>();
-        System.out.println("--- Data Karyawan ---");
+        ArrayList<String> namaPegawai = new ArrayList<>();
+        ArrayList<Integer> GajiPegawai = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        System.out.println("Nama lengkap : ");
-        dataNama.add(input.nextLine());
-        for (String i :dataNama){
-            System.out.println(i);
+        while (true){
+            System.out.println("Nama lengkap :");
+            namaPegawai.add(input.next());
+            System.out.println("Gaji :");
+            GajiPegawai.add(input.nextInt());
+            System.out.println("lanjut isi? ");
+            String next = input.next();
+            if (!Objects.equals(next, "yes")){
+                for (int i = 0; i < GajiPegawai.size(); i++){
+                    System.out.println("Nama : "+namaPegawai.get(i));
+                    System.out.println("gaji : "+namaPegawai.get(i));
+                    System.out.println("-----------------------------");
+                }
+                break;
+            }
         }
 
     }
