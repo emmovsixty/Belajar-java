84f8bed9637dc89dfdbfd49e31b21fefc8c4b2b7
emmovsixty <emmo1933@gmail.com>
2022-08-04 11:34:05 +0700

    modif latihan input

--- src/latihanInput.java
+++ src/latihanInput.java
@@ -1,25 +1,17 @@
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class latihanInput {
     public static void main(String[] args) {
         ArrayList<String> dataNama = new ArrayList<>();
         System.out.println("--- Data Karyawan ---");
-
-        String nama = "farhan";
-        String nama1 = "mualif";
-        String nama2 = "dika";
-        String nama3 = "luth";
-        String nama4 = "saputra";
-        dataNama.add(nama);
-        dataNama.add(nama1);
-        dataNama.add(nama2);
-        dataNama.add(nama3);
-        dataNama.add(nama4);
-
-        for (String i : dataNama){
-            System.out.println("nama "+i);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nama lengkap : ");
+        dataNama.add(input.nextLine());
+        for (String i :dataNama){
+            System.out.println(i);
         }
 
     }
