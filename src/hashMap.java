79260db5ebee5b75a10010bcfbaa138354e1443f
emmovsixty <emmo1933@gmail.com>
2022-08-05 09:31:00 +0700

    modif hashMap

--- src/hashMap.java
+++ src/hashMap.java
@@ -2,18 +2,17 @@ import java.util.HashMap;
 
 public class hashMap {
     public static void main(String[] args) {
-        HashMap<String,Integer> anggota = new HashMap<String,Integer>();
+            HashMap<Integer,String> anggota = new HashMap<Integer,String>();
         String nama;
         Integer umur;
 
-        anggota.put("farhan",19);
-        anggota.put("dimas",21);
-        anggota.put("yusuf",20);
-        anggota.put("bayu",19);
-        anggota.put("baker",19);
-        anggota.put("dwi",24);
-
-        System.out.println(anggota);
-
+        anggota.put(1,"DIMAS");
+        anggota.put(2,"DIKA");
+        anggota.put(3,"ILHAM");
+        anggota.put(4,"DIKI");
+        anggota.put(5,"RESTI");
+        for (int i = 1;i<anggota.size();i++){
+            System.out.println(anggota.get(i));
+        }
     }
 }
